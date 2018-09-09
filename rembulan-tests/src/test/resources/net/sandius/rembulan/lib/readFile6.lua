0618a9c2fc565521c582b3b0986dbea49019c54e
Michael Karneim <unconfigured@null.spigotmc.org>
2018-09-09 20:46:16 +0200

    Remove debug output from test programs

--- rembulan-tests/src/test/resources/net/sandius/rembulan/lib/readFile6.lua
+++ rembulan-tests/src/test/resources/net/sandius/rembulan/lib/readFile6.lua
@@ -5,13 +5,13 @@ local offset2 = tonumber(offsetStr2)
 local file = io.open(filename, 'r')
 
 local pos1 = file:seek(whence1, offset1)
-print("pos1",pos1)
+--print("pos1",pos1)
 local result1 = file:read('*l')
 
 local pos2 = file:seek(whence2, offset2)
-print("pos2",pos2)
+--print("pos2",pos2)
 local result2 = file:read('*l')
 
 io.close(file)
-print(result1,result2)
+--print(result1,result2)
 return result1, result2
