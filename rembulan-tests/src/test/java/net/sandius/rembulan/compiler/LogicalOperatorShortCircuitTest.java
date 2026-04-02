bb9a7233ba23fa2159bf9ea0673a07b75517fd5c
Michael Karneim <mkarneim@googlemail.com>
2026-04-02 23:46:29 +0200

    LogicalOperatorShortCircuitTest: add another regression test

--- rembulan-tests/src/test/java/net/sandius/rembulan/compiler/LogicalOperatorShortCircuitTest.java
+++ rembulan-tests/src/test/java/net/sandius/rembulan/compiler/LogicalOperatorShortCircuitTest.java
@@ -31,4 +31,20 @@ public class LogicalOperatorShortCircuitTest extends TestBase {
     // Then:
     assertThat(actual[0]).isEqualTo("string of success");
   }
+
+  @Test
+  public void test_ternaryIdiom_numericMiddleValue_trueCondition() throws Exception {
+    // Regression: the AND result type is JOIN(BOOLEAN, NUMBER) = NON_NIL.
+    // The old BranchInlinerVisitor incorrectly treated NON_NIL as always-truthy,
+    // causing the OR to be eliminated even though NON_NIL includes false.
+    // Given:
+    String program = "local function f(v) return v == 42 and 0 or v end  return f(42), f(99)";
+
+    // When:
+    Object[] actual = run(program);
+
+    // Then:
+    assertThat(actual[0]).isEqualTo(0L);   // f(42): condition true, should return 0
+    assertThat(actual[1]).isEqualTo(99L);  // f(99): condition false, should return v
+  }
 }
